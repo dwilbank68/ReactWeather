a980b3716121dc9e383a49f36b8faa987de81ad3
David Wilbanks <dwilbank@gmail.com>
2016-09-06 10:03:29 -0400

    add grid

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Examples = (props) => {
     return (
-        <h3>Examples</h3>
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Philadelphia">Philadelphia</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Rio">Rio, Brazil</Link>
+                </li>
+            </ol>
+        </div>
+
     );
 };
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import {IndexLink, Link} from 'react-router';
 
-const Nav = (props) => {
-    return (
-        <div>
-            <h2>Nav Component</h2>
-            <IndexLink to="/"           activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            <Link to="/about"           activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-            <Link to="/examples"        activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-        </div>
+const Nav = React.createClass({
 
-    );
-};
+    onSearch(e){
+        e.preventDefault();
+        console.log('not yet working');
+    },
 
-module.exports = Nav;
+    render() {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            <IndexLink to="/"           activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about"           activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples"        activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <u className="menu">
+                            <li>
+                                <input type="search" placeholder="Search weather"/>
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="Get Weather"/>
+                            </li>
+                        </u>
+                    </form>
+                </div>
+            </div>
+
+        );
+    }
+});
+
+export default Nav;

20dc8c0d4c03a2849b09220118cf8911cea37ca5
David Wilbanks <dwilbank@gmail.com>
2016-09-07 09:05:20 -0400

    add sass loader

--- app/app.jsx
+++ app/app.jsx
@@ -18,7 +18,7 @@ import Examples from 'Examples';
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 
 ReactDOM.render(
